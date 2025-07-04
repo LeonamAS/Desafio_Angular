93632b895f89bf71cb9e7c2ec54cd090d1329fc8
@LeonamAS <leonam.sales@ba.estudante.senai.br>
2025-07-04 09:11:56 -0300

    Segunda Tentativa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
