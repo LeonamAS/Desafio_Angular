bfe962a7489e63c2dfed21f942ab865a8ca52245
LeonamAS <leonam.sales@ba.estudante.senai.br>
2025-07-08 22:36:22 -0300

    Experimentos

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Cabecalho } from "../components/cabecalho/cabecalho";
 import { Rodape } from "../components/rodape/rodape";
+import { Carrossel } from '../components/carrossel/carrossel';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Cabecalho, Rodape],
+  imports: [Cabecalho, Rodape, Carrossel],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
